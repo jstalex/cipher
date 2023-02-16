4efff6f0ad939d89b01d2f824b5422daa5e44917
jstalex <90794457+jstalex@users.noreply.github.com>
2023-02-16 23:47:47 +0300

    add worked version

--- cipher.srcs/sources_1/new/top.sv
+++ cipher.srcs/sources_1/new/top.sv
@@ -59,74 +59,74 @@ logic [7:0] data_galua_shifted  [15:0];
 
 always_ff @(posedge clk_i or negedge resetn_i) begin
     if (~resetn_i) begin
-        state <= IDLE;
-        round <= 0;
+        state = IDLE;
+        round = 0;
         temp_data = 0;
         xored_data = 0;
-        counter <= 0;
-        busy_o <= 0;
-        valid_o <= 0;
+        counter = 0;
+        busy_o = 0;
+        valid_o = 0;
     end else begin
         case(state)
         IDLE: begin
             if (request_i) begin
-                temp_data <= data_i;
-                counter <= 0;
-                round <= 0;
-                valid_o <= 0;
-                busy_o <= 1;
-                state <= KEY;
+                temp_data = data_i;
+                counter = 0;
+                round = 0;
+                valid_o = 0;
+                busy_o = 1;
+                state = KEY;
             end
         end
         KEY: begin
-            xored_data <= temp_data ^ key_mem[round];
+            xored_data = temp_data ^ key_mem[round];
             
-            data_key_result_bytes[0] <= xored_data[7:0];
-            data_key_result_bytes[1] <= xored_data[15:8];
-            data_key_result_bytes[2] <= xored_data[23:16];
-            data_key_result_bytes[3] <= xored_data[31:24];
-            data_key_result_bytes[4] <= xored_data[39:32];
-            data_key_result_bytes[5] <= xored_data[47:40];
-            data_key_result_bytes[6] <= xored_data[55:48];
-            data_key_result_bytes[7] <= xored_data[63:56];
-            data_key_result_bytes[8] <= xored_data[71:64];
-            data_key_result_bytes[9] <= xored_data[79:72];
-            data_key_result_bytes[10] <= xored_data[87:80];
-            data_key_result_bytes[11] <= xored_data[95:88];
-            data_key_result_bytes[12] <= xored_data[103:96];
-            data_key_result_bytes[13] <= xored_data[111:104];
-            data_key_result_bytes[14] <= xored_data[119:112];
-            data_key_result_bytes[15] <= xored_data[127:120];
+            data_key_result_bytes[0] = xored_data[7:0];
+            data_key_result_bytes[1] = xored_data[15:8];
+            data_key_result_bytes[2] = xored_data[23:16];
+            data_key_result_bytes[3] = xored_data[31:24];
+            data_key_result_bytes[4] = xored_data[39:32];
+            data_key_result_bytes[5] = xored_data[47:40];
+            data_key_result_bytes[6] = xored_data[55:48];
+            data_key_result_bytes[7] = xored_data[63:56];
+            data_key_result_bytes[8] = xored_data[71:64];
+            data_key_result_bytes[9] = xored_data[79:72];
+            data_key_result_bytes[10] = xored_data[87:80];
+            data_key_result_bytes[11] = xored_data[95:88];
+            data_key_result_bytes[12] = xored_data[103:96];
+            data_key_result_bytes[13] = xored_data[111:104];
+            data_key_result_bytes[14] = xored_data[119:112];
+            data_key_result_bytes[15] = xored_data[127:120];
             
             if (round == 9) begin
-                data_o <= xored_data;
-                busy_o <= 0;
-                valid_o <= 1;
-                state <= FINISH;
+                data_o = xored_data;
+                busy_o = 0;
+                valid_o = 1;
+                state = FINISH;
             end else begin
-                state <= S;
+                state = S;
             end
         end
         S: begin
-            data_linear_result[0] <= S_box_mem[data_key_result_bytes[0]];
-            data_linear_result[1] <= S_box_mem[data_key_result_bytes[1]]; 
-            data_linear_result[2] <= S_box_mem[data_key_result_bytes[2]]; 
-            data_linear_result[3] <= S_box_mem[data_key_result_bytes[3]]; 
-            data_linear_result[4] <= S_box_mem[data_key_result_bytes[4]]; 
-            data_linear_result[5] <= S_box_mem[data_key_result_bytes[5]]; 
-            data_linear_result[6] <= S_box_mem[data_key_result_bytes[6]]; 
-            data_linear_result[7] <= S_box_mem[data_key_result_bytes[7]]; 
-            data_linear_result[8] <= S_box_mem[data_key_result_bytes[8]]; 
-            data_linear_result[9] <= S_box_mem[data_key_result_bytes[9]]; 
-            data_linear_result[10] <= S_box_mem[data_key_result_bytes[10]]; 
-            data_linear_result[11] <= S_box_mem[data_key_result_bytes[11]]; 
-            data_linear_result[12] <= S_box_mem[data_key_result_bytes[12]]; 
-            data_linear_result[13] <= S_box_mem[data_key_result_bytes[13]]; 
-            data_linear_result[14] <= S_box_mem[data_key_result_bytes[14]];
-            data_linear_result[15] <= S_box_mem[data_key_result_bytes[15]];
+            data_linear_result[0] = S_box_mem[data_key_result_bytes[0]];
+            data_linear_result[1] = S_box_mem[data_key_result_bytes[1]]; 
+            data_linear_result[2] = S_box_mem[data_key_result_bytes[2]]; 
+            data_linear_result[3] = S_box_mem[data_key_result_bytes[3]]; 
+            data_linear_result[4] = S_box_mem[data_key_result_bytes[4]]; 
+            data_linear_result[5] = S_box_mem[data_key_result_bytes[5]]; 
+            data_linear_result[6] = S_box_mem[data_key_result_bytes[6]]; 
+            data_linear_result[7] = S_box_mem[data_key_result_bytes[7]]; 
+            data_linear_result[8] = S_box_mem[data_key_result_bytes[8]]; 
+            data_linear_result[9] = S_box_mem[data_key_result_bytes[9]]; 
+            data_linear_result[10] = S_box_mem[data_key_result_bytes[10]]; 
+            data_linear_result[11] = S_box_mem[data_key_result_bytes[11]]; 
+            data_linear_result[12] = S_box_mem[data_key_result_bytes[12]]; 
+            data_linear_result[13] = S_box_mem[data_key_result_bytes[13]]; 
+            data_linear_result[14] = S_box_mem[data_key_result_bytes[14]];
+            data_linear_result[15] = S_box_mem[data_key_result_bytes[15]];
             
-            temp_L_data <= data_linear_result;
-            state <= L;   
+            temp_L_data = data_linear_result;
+            state = L;   
         end
         L: begin
             if (counter < 16) begin
@@ -147,31 +147,48 @@ always_ff @(posedge clk_i or negedge resetn_i) begin
                 data_galua_result[1]  = L_mul_148_mem [temp_L_data[14]]; 
                 data_galua_result[0]  =                temp_L_data[15] ;
                 
-                galua_summ <= data_galua_result[0] ^ data_galua_result[1] ^ data_galua_result[2] ^ data_galua_result[3] ^ data_galua_result[4] ^ data_galua_result[5] ^ data_galua_result[6] ^ data_galua_result[7] ^ data_galua_result[8] ^ data_galua_result[9] ^ data_galua_result[10] ^ data_galua_result[11] ^ data_galua_result[12] ^ data_galua_result[13] ^ data_galua_result[14] ^ data_galua_result[15];
+                galua_summ = ((((((((((((((data_galua_result[0] ^ data_galua_result[1]) ^ data_galua_result[2]) ^ data_galua_result[3]) ^ data_galua_result[4]) ^ data_galua_result[5]) ^ data_galua_result[6]) ^ data_galua_result[7]) ^ data_galua_result[8]) ^ data_galua_result[9]) ^ data_galua_result[10]) ^ data_galua_result[11]) ^ data_galua_result[12]) ^ data_galua_result[13]) ^ data_galua_result[14]) ^ data_galua_result[15];
                 
-                data_galua_shifted <= {galua_summ, data_galua_result[15:1]};
-                temp_L_data <= data_galua_shifted;
+                data_galua_shifted[15] = galua_summ;
+                data_galua_shifted[14:0] = data_galua_result[15:1];
+                temp_L_data = data_galua_shifted;
                 
-                counter <= counter + 1;
+                counter = counter + 1;
             end else begin
-                temp_data <= {temp_L_data[7], temp_L_data[6], temp_L_data[5], temp_L_data[4], temp_L_data[3], temp_L_data[2], temp_L_data[1], temp_L_data[0]};
-                counter <= 0;
-                round <= round + 1;
-                state <= KEY;
+                temp_data[7:0] = temp_L_data[15];
+                temp_data[15:8] = temp_L_data[14];
+                temp_data[23:16] = temp_L_data[13];
+                temp_data[31:24] = temp_L_data[12];
+                temp_data[39:32] = temp_L_data[11];
+                temp_data[47:40] = temp_L_data[10];
+                temp_data[55:48] = temp_L_data[9];
+                temp_data[63:56] = temp_L_data[8];
+                temp_data[71:64] = temp_L_data[7];
+                temp_data[79:72] = temp_L_data[6];
+                temp_data[87:80] = temp_L_data[5];
+                temp_data[95:88] = temp_L_data[4];
+                temp_data[103:96] = temp_L_data[3];
+                temp_data[111:104] = temp_L_data[2];
+                temp_data[119:112] = temp_L_data[1];
+                temp_data[127:120] = temp_L_data[0];
+                counter = 0;
+                round = round + 1;
+                state = KEY;
             end
         end
         FINISH: begin
             if (request_i) begin
-                temp_data <= data_i;
+                temp_data = data_i;
                 counter = 0;
                 round = 0;
                 
-                busy_o <= 1;
-                valid_o <= 0;
+                busy_o = 1;
+                valid_o = 0;
                 
-                state <= KEY;
+                state = KEY;
             end else if (ack_i) begin
-                state <= IDLE;
+                valid_o = 0;
+                state = IDLE;
             end
         end
         endcase
